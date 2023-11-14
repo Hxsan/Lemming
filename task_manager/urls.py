897e8ab62f3bb019e8afd361023a7fe23187b35c
Hasan <mohammed.6.ahmed@kcl.ac.uk>
2023-11-14 13:36:22 +0000

    Fix syntax - Add Square Bracket

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('create_task/', views.CreateTaskView.as_view(), name='create_task'),
     path('search_users/', views.search_users, name='search_users'),
+]
 

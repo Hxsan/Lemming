b5bfe0b8b4cb61acf20365049d5bc21dc955cb78
Issa Abdi <issa.abdi@kcl.ac.uk>
2023-11-30 19:04:06 +0000

    finish tests for assign task form

--- tasks/tests/forms/test_assign_task_form.py
+++ tasks/tests/forms/test_assign_task_form.py
@@ -3,39 +3,40 @@ from django import forms
 from django.test import TestCase
 from django.urls import reverse
 from tasks.forms import AssignTaskForm
-from tasks.models import User, Task
+from tasks.models import User, Task, Team
 
 class AssignTaskFormTestCase(TestCase):
      
-    fixtures = ['tasks/tests/fixtures/default_user.json', 'tasks/tests/fixtures/other_users.json']
+    fixtures = [
+        'tasks/tests/fixtures/default_user.json', 
+        'tasks/tests/fixtures/other_users.json',
+        'tasks/tests/fixtures/default_team.json',
+    ]
 
     def setUp(self):
-        self.url = reverse('assign_task')
         self.user = User.objects.get(username='@johndoe')
         self.second_user = User.objects.get(username='@janedoe')
         self.third_user = User.objects.get(username='@petrapickles')
+        self.team = Team.objects.get(team_name='Team 1')
         self.task = Task.objects.create(
             title='Task 1',
             description='This is a task',
             due_date='2024-02-01',
-            created_by=self.user,
+            created_by=self.team,
         )
+        self.url = reverse('view_task', kwargs={'team_id': self.team.pk, 'task_id': self.task.pk})
         self.form_input = {
             'usernames' : ['@janedoe'],
+            'assign_submit': '',
         }
     
     def test_valid_assign_task_form(self):
-        form = AssignTaskForm(data=self.form_input)
+        form = AssignTaskForm(self.team, self.task, data=self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_assign_task_form_has_necessary_fields(self):
-        form = AssignTaskForm()
+        form = AssignTaskForm(self.team, self.task)
         self.assertIn('usernames', form.fields)
-
-    def test_usernames_selected_must_not_be_zero(self):
-        self.form_input['usernames'] = ''
-        form = AssignTaskForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
     
     def test_assign_task_must_save_correctly(self):
         self.client.login(username=self.user.username, password='Password123')
@@ -43,12 +44,12 @@ class AssignTaskFormTestCase(TestCase):
         self.client.post(self.url, self.form_input)
         self.task.refresh_from_db()
         after_count = self.task.assigned_to.count()
-        self.assertEqual(after_count, before_count+1) # Expected to fail, assign task is unfinished
+        self.assertEqual(after_count, before_count+1)
 
     def test_assign_task_to_yourself_is_valid(self):
         self.client.login(username=self.user.username, password='Password123')
         self.form_input['usernames'] = ['@johndoe']
-        form = AssignTaskForm(data=self.form_input)
+        form = AssignTaskForm(self.team, self.task, data=self.form_input)
         self.assertTrue(form.is_valid())
     
     def test_assign_multiple_users_to_task_saves_correctly(self):
@@ -56,6 +57,5 @@ class AssignTaskFormTestCase(TestCase):
         self.form_input['usernames'] = ['@janedoe', '@peterpickles', '@petrapickles']
         before_count = self.task.assigned_to.count()
         self.client.post(self.url, self.form_input)
-        self.task.refresh_from_db()
         after_count = self.task.assigned_to.count()
-        self.assertEqual(after_count, before_count+3) # Expected to fail, assign task is unfinished
+        self.assertEqual(after_count, before_count+3)
